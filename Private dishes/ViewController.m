4a2355822a0743757beaefcce50ea31fb2550c00
Panerly <panerly@panerlydeMacBook-Air.local>
2017-07-10 11:00:50 +0800

    Initial Commit

---
+++ Private dishes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Private dishes
+//
+//  Created by panerly on 10/07/2017.
+//  Copyright © 2017 panerly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Private dishes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Private dishes
+//
+//  Created by panerly on 10/07/2017.
+//  Copyright © 2017 panerly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
